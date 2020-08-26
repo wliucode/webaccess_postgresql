036c4349c4230307faabfedab5c0d0b1aec35880
wliu <williamliu_usa@yahoo.com>
2020-08-26 01:34:29 -0700

    - Create DataSource for PostgreSQL - Update pom.xml - Remove application.properties - Add application.yml in order to inject url, user name, password which allow us to connect to PostgresSQL database -  Create V1__PersonTable.sql

---
+++ src/main/resources/db/migration/V1__PersonTable.sql
@@ -0,0 +1,7 @@
+CREATE TABLE person (
+    id UUID NOT NULL PRIMARY KEY,
+    firstName VARCHAR(100) NOT NULL,
+    lastName VARCHAR(100) NOT NULL,
+    email   VARCHAR(100) NOT NULL,
+    password VARCHAR(100) NOT NULL
+)
